2357a81dfb32796121ebb2aca80ff13bca3c2aaa
0ric <oricricardo@gmail.com>
2023-03-28 18:43:25 -0300

    editando as rotas

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,15 +1,11 @@
 Rails.application.routes.draw do
+ # namspace é o nome da pasta 
+ # resources pega todo o conteudo do controller
+
   namespace :api do
     namespace :v1 do
-      get 'travels/index'
-      get 'travels/show'
-      get 'travels/create'
-      get 'travels/update'
-      get 'travels/destroy'
+        resources :travels
     end
   end
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
 end

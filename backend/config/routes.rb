65dbe3ac33bcb0ca51c6493650562b6b9899dc90
0ric <oricricardo@gmail.com>
2023-03-28 18:35:48 -0300

    controller criado

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      get 'travels/index'
+      get 'travels/show'
+      get 'travels/create'
+      get 'travels/update'
+      get 'travels/destroy'
+    end
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")

3c2d77dd5db62e43578244ee22118481b6d53dcc
0ric <oricricardo@gmail.com>
2023-04-11 18:41:59 -0300

    pegando os dados da api

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,21 @@
+import axios from 'axios'
+import {useState,useEffect} from 'react'
+
 function App() {
+    const [Viagens,setViagens] = useState([]);
+
+    useEffect(()=>{
+        axios.get("http://localhost:8080/api/v1/travels")
+            .then(res=>setViagens(res.data))
+            .catch(erro=> console.log("deu erro no getApi: ",erro))
+
+    },[])
+
     return (
         <div>App</div>
     )
 }
+
+
+
 export default App

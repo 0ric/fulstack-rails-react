37e8670bc5524e638cda1d3d28754983c5b082a2
0ric <oricricardo@gmail.com>
2023-04-11 19:29:58 -0300

    fomulario de cadastro incompleto

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,12 @@ import {useState,useEffect} from 'react'
 
 function App() {
     const [Viagens,setViagens] = useState([]);
+    const [travel,setTravel] = useState({ 
+        nome: '',
+        data: '',
+        desc: '',
+        price: 0
+    })
 
     useEffect(()=>{
         axios.get("http://localhost:8080/api/v1/travels")
@@ -11,8 +17,32 @@ function App() {
 
     },[])
 
+    const cadastrarViagem=(travel)=>{
+        axios.post("http://localhost:8080/api/v1/travels",{travel})
+            .then(res=>{
+                console.log(res);
+                setViagens([...Viagens,res.data])
+            })
+        
+    
+    } 
+    function EnvioFormulario(e){
+        e.preventDefault()
+        cadastrarViagem(travel)
+     }
     return (
         <div>
+            <form onSubmit={EnvioFormulario}>
+                <label>Nome</label>
+                <input
+                id="nome"
+                required
+                onChange={(e)=>setTravel({...travel,nome: e.target.value})}
+                value={travel.nome}
+                placeholder="Digite o nome da viagem"
+                />
+            <button type='submit'>Cadastrar viagem</button>
+            </form>
            {
             Viagens.map(v=>
                 <h1>{v.nome}</h1>

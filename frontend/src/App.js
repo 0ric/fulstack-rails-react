dfe105f5b31f34b0e7179986707ce16ff9a7711b
JoaoWebMeneses <joaowebmeneses@gmail.com>
2023-04-18 18:35:02 -0300

    post finalizado

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -41,6 +41,32 @@ function App() {
                 value={travel.nome}
                 placeholder="Digite o nome da viagem"
                 />
+                <label>Data:</label>
+                <input
+                    id="data"
+                    required
+                    onChange={(e)=>setTravel({...travel,data: e.target.value})}
+                    value={travel.data}
+                    placeholder='Data'
+                    type='datetime-local'                
+                />
+                <label>Descrição:</label>
+                <textarea
+                    required
+                    id='desc'
+                    onChange={(e)=>setTravel({...travel,desc: e.target.value})}
+                    value={travel.desc}
+                    placeholder='descrição da viagem'
+                />
+                <label>Price:</label>
+                 <input
+                    required
+                    id="price"
+                    onChange={(e)=>setTravel({...travel,price: e.target.value})}
+                    value={travel.price}
+                    placeholder='Preço'
+                    type='number'
+                />
             <button type='submit'>Cadastrar viagem</button>
             </form>
            {

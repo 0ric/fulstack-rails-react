b9abf8314f856f4d095a4b3daa8857c03100ef7b
0ric <oricricardo@gmail.com>
2023-04-25 18:52:17 -0300

    css do card ok

--- frontend/src/exemplos/components/Card.jsx
+++ frontend/src/exemplos/components/Card.jsx
@@ -1,9 +1,13 @@
 import "./Card.css"
-
+import {TbTrashX} from 'react-icons/tb'
 function Card(props) {
 return (
     <div className="card">
         <h1>{props.nome}</h1>
+            <div >
+            <TbTrashX size={32}/>
+            
+            </div>
     </div>
 )
 }

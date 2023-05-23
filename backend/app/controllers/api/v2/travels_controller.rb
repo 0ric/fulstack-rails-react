d1f2c7cad7bbd34150b6ab773151345c2bed80e8
0ric <oricricardo@gmail.com>
2023-05-23 19:08:22 -0300

    controller da v2 ok

---
+++ backend/app/controllers/api/v2/travels_controller.rb
@@ -0,0 +1,40 @@
+class Api::V2::TravelsController < ApplicationController
+  def index
+    @travels = Travel.all
+    render json: @travels
+  end
+
+  def show
+    @travel = Travel.find(params[id])
+    render json: @travel
+  end
+
+  def create
+    @travel = Travel.new(travel_params)
+    if @travel.save
+      render json: @travel
+    else
+      render json: {errors: @travel.errors, message: "não foi possivel cadastrar"},status: :unprocessable_entity
+    end
+  end
+
+  def update
+    @travel = Travel.find(params[:id])
+    if @travel.update(travel_params)
+      render json: @travel
+    else
+      render json: {message: "não foi possivel atualizar"},status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @travel = Travel.find(params[:id])
+    @travel.destroy
+    render json: {message: "Sua viagem foi deletada com sucesso :)"}
+  end
+
+  private
+    def travel_params
+      params.require(:travel).permit( :nome, :data, :price, :desc)
+    end
+end

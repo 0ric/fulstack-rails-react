8b54ef1e5dfaaf772463f3b8adf56c7c436e8e9d
0ric <oricricardo@gmail.com>
2023-05-23 19:23:32 -0300

    url padrao

--- backend/app/controllers/api/v2/travels_controller.rb
+++ backend/app/controllers/api/v2/travels_controller.rb
@@ -35,6 +35,6 @@ class Api::V2::TravelsController < ApplicationController
 
   private
     def travel_params
-      params.require(:travel).permit( :nome, :data, :price, :desc)
+      params.require(:travel).permit( :nome, :data, :price, :desc,:image)
     end
 end
--- backend/config/environments/development.rb
+++ backend/config/environments/development.rb
@@ -1,5 +1,9 @@
 require "active_support/core_ext/integer/time"
 
+Rails.application.routes.default_url_options = {
+  host: 'http://localhost:8080'
+}
+]
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 

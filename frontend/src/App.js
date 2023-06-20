4f4f3e85e3e2bc92eca210fb71409d8193237217
0ric <oricricardo@gmail.com>
2023-06-20 19:11:02 -0300

    navbar com imagem

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,10 @@ import {useState,useEffect} from 'react'
 import Form from './components/Form';
 import Card from './components/Card';
 import Popup from './components/Popup';
+import Logo from './imagens/cover.png'
+
+
+
 function App() {
     const [Viagens,setViagens] = useState([]);
     const [travel,setTravel] = useState({ 
@@ -61,6 +65,7 @@ function App() {
 
         })
     } 
+
     const deleteTravel = (id)=>{
         axios.delete(`http://localhost:8080/api/v2/travels/${id}`)
         .then(res=>{
@@ -139,6 +144,33 @@ function App() {
      }
     return (
             <div>
+                
+                <nav class="navbar bg-dark border-bottom border-bottom-dark" data-bs-theme="dark">
+                    <div class="container-fluid">                      
+                        <a class="navbar-brand" href="#"></a>
+                        <img src= {Logo} alt="Logo" width="350" height="120" class="d-inline-block align-text-top"/>
+
+                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="collapse navbar-collapse" id="navbarNav">
+                        <ul class="navbar-nav">
+                            <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#"></a>
+                            </li>
+                            <li class="nav-item">
+                            <a class="nav-link" href="#"></a>
+                            </li>
+                            <li class="nav-item">
+                            <a class="nav-link" href="#"></a>
+                            </li>
+                            <li class="nav-item">
+                            <a class="nav-link disabled"></a>
+                            </li>
+                        </ul>
+                        </div>
+                    </div>
+                    </nav>
         <Form
             id={editID}
         
